b3788b2f04115d74ff27a52ea85cf4ad56071e42
Luis Alfredo Palomino Medina <pmpalominomedina@gmail.com>
2025-10-22 23:23:25 -0500

    implementacion de pedido

--- src/models/pedidos.js
+++ src/models/pedidos.js
@@ -2,10 +2,11 @@ const { Schema, model } = require("mongoose");
 
 const pedidosSchema = new Schema(
   {
-    producto: {type: Array, require: true,},
     nombreCliente: {type: String,},
+    producto: {type: Array, require: true,},
     rucCliente: {type: String,},
     numeroCotizacion: {type: String,},
+    fechaPedido: {type: Date}
   },
   {
     timestamps: true,
